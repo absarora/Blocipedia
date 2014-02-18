b792f106ca8aad5a6cd4da8974eb5423f003a787
absarora <absingharora@gmail.com>
2014-02-18 02:24:23 +0000

    First commit and README update

---
+++ README.md
@@ -0,0 +1,7 @@
+README
+
+**Saas Wiki Collaboration Tool**  
+
+Purpose  
+
+Build an application that allows users to create public and private Markdown-based wikis.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blocipedia::Application.config.secret_token = 'be6625ba62a7aa38c6f2d01d3cec51deea43910b54dd1ca562068360d65c8ab93528aefb57002e5e0e52b78ac42fe77bb8a7f591fe1af71b82d449ddcc065a1e'
